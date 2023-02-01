975da5027ed090d8e0974d99d97ced05ede826ad
Gurgen <gsarkisdganyan@gmail.com>
2023-02-01 22:12:11 +0300

    add build config by using env

--- config/build/types/config.ts
+++ config/build/types/config.ts
@@ -5,8 +5,15 @@ export interface BuildPath {
     build: string;
     html: string;
 }
+
+export interface BuildEnv {
+    mode: BuildMode,
+    port: number
+}
+
 export interface BuildOptions {
     mode: BuildMode;
     paths: BuildPath;
     isDev: boolean;
+    port: number
 }
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 import {someFunction} from './test';
 someFunction(6);
+document.body.innerHTML = `<div>HELLO WEBPACK</div>`;
