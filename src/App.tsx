6df98dee235655948b5d5997e55325441b3f522a
Gurgen <gsarkisdganyan@gmail.com>
2023-02-02 21:53:02 +0300

    add react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
-import {Counter} from "./components/Counter";
+import React, { Suspense } from 'react';
+import {Link, Route, Routes} from 'react-router-dom';
 import './index.scss';
+
+const HomeAsync = React.lazy(() => import('./pages/Home'))
+const AboutAsync = React.lazy(() => import('./pages/About'))
+
 
 const App = () => {
     return (
         <div className='app'>
-            Some test file
-            <Counter/>
+            <Link to={'/home'}>Home</Link>
+            <Link to={'/about'}>About</Link>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path={'/home'} element={<HomeAsync/>}/>
+                    <Route path={'/about'} element={<AboutAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
