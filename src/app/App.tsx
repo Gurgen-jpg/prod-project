b990df80afbd5c0ab41fe01bf97539301b2856a1
Gurgen <gsarkisdganyan@gmail.com>
2023-02-07 21:55:36 +0300

    - refactor routes

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,9 @@
-import React, {Suspense} from 'react';
-import {Link, Route, Routes} from 'react-router-dom';
+import React from 'react';
+import {Link} from 'react-router-dom';
 import {useTheme} from "app/providers/ThemeProvider";
 import {classNames} from 'shared/classNames/classNames';
 import './../app/styles/index.scss';
-import {AboutPage} from 'pages/AboutPage';
-import {HomePage} from "pages/HomePage";
+import {AppRouter} from "app/providers/router";
 
 
 const App = () => {
@@ -15,12 +14,7 @@ const App = () => {
             <button onClick={toggleTheme}>theme</button>
             <Link to={'/home'}>Home</Link>
             <Link to={'/about'}>About</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/home'} element={<HomePage/>}/>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
