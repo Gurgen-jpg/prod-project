c38b781eb1f318fa6353b2646e08ac01b1987cb5
Gurgen <gsarkisdganyan@gmail.com>
2023-03-11 23:29:38 +0300

    - add extra in thunk in test class

--- src/features/AuthByUserName/model/services/loginByUserName/loginByUserName.test.ts
+++ src/features/AuthByUserName/model/services/loginByUserName/loginByUserName.test.ts
@@ -5,8 +5,6 @@ import { loginByUserName } from './loginByUserName';
 
 jest.mock('axios');
 
-const mockedAxios = jest.mocked(axios, true);
-
 describe('loginByUserName.test', () => {
     // let dispatch: Dispatch;
     // let getState: () => StateSchema;
@@ -38,25 +36,25 @@ describe('loginByUserName.test', () => {
     // });
 
     test('success login', async () => {
-        const userValue = { username: '123', id: '1' };
-        mockedAxios.post.mockReturnValue(Promise.resolve({ data: userValue }));
-
         const thunk = new TestAsyncThunk(loginByUserName);
+        const userValue = { username: '123', id: '1' };
+
+        thunk.api.post.mockReturnValue(Promise.resolve({ data: userValue }));
         const result = await thunk.callThunk({ username: 'name', password: '123' });
 
         expect(thunk.dispatch).toHaveBeenCalledWith(userActions.setAuthData(userValue));
         expect(thunk.dispatch).toHaveBeenCalledTimes(3);
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('fulfilled');
         expect(result.payload).toEqual(userValue);
     });
     test('error login', async () => {
-        mockedAxios.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const thunk = new TestAsyncThunk(loginByUserName);
+        thunk.api.post.mockReturnValue(Promise.resolve({ status: 403 }));
         const result = await thunk.callThunk({ username: 'name', password: '123' });
 
         expect(thunk.dispatch).toHaveBeenCalledTimes(2);
-        expect(mockedAxios.post).toHaveBeenCalled();
+        expect(thunk.api.post).toHaveBeenCalled();
         expect(result.meta.requestStatus).toBe('rejected');
         expect(result.payload).toEqual('error');
     });
